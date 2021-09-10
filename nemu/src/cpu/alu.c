7b7293b14f1901a74f5fc2ea4e5ca6bf6fc297b7
tracer-pa-public <tracer@njuics.org>
2021-09-10 23:15:25 +0800

    > clean > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 23:15:25 up 15 days, 12:18,  2 users,  load average: 2.74, 3.16, 3.00 > unitime > 6e807e12cf09a2dd2eb5228b87bd22f33347c60b

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -5,10 +5,12 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = 0;
+	res = dest + src; //获取计算结果
+	
+	
+	
+	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
 
