8506812ea96aa7615cec726044a9d6b1efaa4f3f
tracer-pa-public <tracer@njuics.org>
2021-09-11 18:23:23 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 18:23:23 up 16 days,  7:26,  2 users,  load average: 3.13, 3.11, 2.93 > unitime 1631355803127 > 102f763276e9d37f51ea9f060c45d9ac4a5d2fb

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -88,7 +88,7 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sbb(src, dest, data_size);
 #else
-    printf("dest = %X, src = %X, CF = X%,  ", dest, src, cpu.eflags.CF);
+    printf("dest = %X, src = %X, CF = %X,  ", dest, src, cpu.eflags.CF);
     src = alu_adc(src, 0, data_size);
     bool cf = cpu.eflags.CF;
 	uint32_t res = alu_sub(src, dest, data_size);
