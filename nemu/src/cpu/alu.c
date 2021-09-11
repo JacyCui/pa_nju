d37f780147a714d1a89cb02a852dea7d4ad6d794
tracer-pa-public <tracer@njuics.org>
2021-09-11 14:20:55 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 14:20:55 up 16 days,  3:23,  2 users,  load average: 2.59, 2.72, 2.80 > unitime 1631341254911 > 88f95c9982a51cf9f712f4b33d3be0ff02dbfb4

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -8,6 +8,13 @@ void set_CF_add(uint32_t result, uint32_t src, size_t data_size)
     cpu.eflags.CF = result < src;
 }
 
+void set_CF_adc(uint32_t result, uint32_t src, size_t data_size)
+{
+    result = sign_ext(resize(result, data_size), data_size);
+    src = sign_ext(resize(src, data_size), data_size);
+    cpu.eflags.CF = result <= src;
+}
+
 void set_ZF(uint32_t result, size_t data_size)
 {
     result = resize(result, data_size);
@@ -62,7 +69,14 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 	return __ref_alu_adc(src, dest, data_size);
 #else
 	uint32_t carry = resize(cpu.eflags.CF, data_size);
-    return alu_add(src + carry, dest, data_size);
+    uint32_t res = dest + src + carry; // Calculate the Result
+	// set flags
+	set_PF(res);
+	set_CF_adc(res, src, data_size);
+	set_ZF(res, data_size);
+	set_SF(res, data_size);
+	set_OF_add(res, src, dest, data_size);
+	return resize(res, data_size);
 #endif
 }
 
