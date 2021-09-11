0771e0efe8eecea5947cb5fb013663877e00224f
tracer-pa-public <tracer@njuics.org>
2021-09-12 01:05:02 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 01:05:02 up 16 days, 14:07,  2 users,  load average: 2.34, 2.56, 2.76 > unitime 1631379901934 > 1d8490143226a2635d4c7bb369434d89d2350325

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -110,8 +110,8 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_mul(src, dest, data_size);
 #else
-	
-	return 0;
+	uint64_t res = src * dest;
+	return res;
 #endif
 }
 
