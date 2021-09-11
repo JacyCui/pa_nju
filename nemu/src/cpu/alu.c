1478dc2d90307072a77c904fd9c0116702743636
tracer-pa-public <tracer@njuics.org>
2021-09-12 00:15:43 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 00:15:43 up 16 days, 13:18,  2 users,  load average: 2.29, 2.71, 2.91 > unitime 1631376943028 > a33796b9cc70c57e864e63c331338d80e06687df

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -230,8 +230,8 @@ uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
 #else
     dest = resize(dest, data_size);
 	uint32_t res = (uint32_t)dest >> src; // Calculate the Result
-	//uint32_t ref_res = __ref_alu_shr(src, dest, data_size);
-	//printf("dest = %x, src = %d, data_size = %d, res = %x; ref = %x\n", dest, src, data_size, res, ref_res);
+	uint32_t ref_res = __ref_alu_shr(src, dest, data_size);
+	printf("dest = %x, src = %d, data_size = %d, res = %x; ref = %x\n", dest, src, data_size, res, ref_res);
 	set_flags(res, src, dest, data_size, SHR); // set flags
 	return resize(res, data_size);
 #endif
