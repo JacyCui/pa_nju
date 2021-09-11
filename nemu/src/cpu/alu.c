4cd85295d5cddc24eb27b7e95fc8154e402a6c95
tracer-pa-public <tracer@njuics.org>
2021-09-11 14:18:41 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 14:18:41 up 16 days,  3:21,  2 users,  load average: 2.61, 2.77, 2.83 > unitime 1631341121816 > 8d5378b27078b678567e6c929e74b232df2a6a4a

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -45,8 +45,7 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
-	uint32_t res = 0;
-	res = dest + src; // Calculate the Result
+	uint32_t res = dest + src; // Calculate the Result
 	// set flags
 	set_PF(res);
 	set_CF_add(res, src, data_size);
@@ -63,7 +62,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 	return __ref_alu_adc(src, dest, data_size);
 #else
 	uint32_t carry = resize(cpu.eflags.CF, data_size);
-    return alu_add(alu_add(src, carry, data_size), dest, data_size);
+    return alu_add(src + carry, dest, data_size);
 #endif
 }
 
