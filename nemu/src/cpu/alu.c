92ed396bef303344819a6c7a20b130680079af01
tracer-pa-public <tracer@njuics.org>
2021-09-11 17:50:22 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 17:50:22 up 16 days,  6:53,  2 users,  load average: 2.65, 2.75, 2.81 > unitime 1631353821985 > e7345ee3bc2b2376ae43073a2949dbd7de1e9d38

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -89,12 +89,12 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 	return __ref_alu_sbb(src, dest, data_size);
 #else
     src = alu_adc(src, 0, data_size);
-    bool cf = cpu.eflags.CF;//, of = cpu.eflags.OF;
+    bool cf = cpu.eflags.CF, of = cpu.eflags.OF;
     //printf("dest = %X, src = %X, OF = %X, ", dest, src, cpu.eflags.OF);
 	uint32_t res = alu_sub(src, dest, data_size);
 	//printf("OF' = %X\n", cpu.eflags.OF);
 	cpu.eflags.CF = cf || cpu.eflags.CF;
-	//cpu.eflags.OF = of || cpu.eflags.OF;
+	cpu.eflags.OF = of || cpu.eflags.OF;
 	return res;
 #endif
 }
