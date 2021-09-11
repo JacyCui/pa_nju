dc3eada494b7771d2038b4a522ab23cf7032d618
tracer-pa-public <tracer@njuics.org>
2021-09-11 19:04:09 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 19:04:09 up 16 days,  8:06,  2 users,  load average: 3.24, 3.05, 2.92 > unitime 1631358249430 > 22027a5c97f29ca61b7566626a114e8d1bf9f4b

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -88,21 +88,12 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sbb(src, dest, data_size);
 #else
-    uint32_t cf = cpu.eflags.CF;
-    /*
-    if (cpu.eflags.CF)
-    {
-        printf("dest = %X, src = %X, CF = %X,  ", dest, src, cpu.eflags.CF);
-        src = alu_add(src, 1, data_size);
-        bool cf = cpu.eflags.CF;
-        printf("CF1 = %X, OF1 = %X,  ",  cpu.eflags.CF, cpu.eflags.OF);
-	    uint32_t res = alu_sub(src, dest, data_size);
-	    cpu.eflags.CF = cf || cpu.eflags.CF;
-	    printf("CF2 = %X, OF2 = %X\n", cpu.eflags.CF, cpu.eflags.OF);
-	    return res;
-    }
-    */
-    return alu_sub(cf, alu_sub(src, dest, data_size), data_size);
+    src = alu_add(cpu.eflags, src, data_size);
+    bool cf = cpu.eflags.CF, of = cpu.eflags.OF;
+    uint32_t res = alu_sub(src, dest, data_size);
+    cpu.eflags.CF = cf && cpu.eflags.CF;
+    cpu.eflags.OF = of && cpu.eflags.OF;
+    return res;
 #endif
 }
 
