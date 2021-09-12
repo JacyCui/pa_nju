448cd0f8a0999b9847fcaa5132c8dfc4c6a773a6
tracer-pa-public <tracer@njuics.org>
2021-09-12 10:01:16 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 10:01:16 up 16 days, 23:03,  2 users,  load average: 2.36, 2.60, 2.74 > unitime 1631412076406 > 28acb328d40da583883496b822154e28b3cf960

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -143,10 +143,7 @@ int32_t alu_idiv(int64_t src, int64_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_idiv(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	return resize(dest / src, data_size);
 #endif
 }
 
@@ -164,10 +161,7 @@ int32_t alu_imod(int64_t src, int64_t dest)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_imod(src, dest);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	return dest % src;
 #endif
 }
 
