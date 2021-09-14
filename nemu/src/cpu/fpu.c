b0da2a40a7cd8dca60a45f71614a4f896349c519
tracer-pa-public <tracer@njuics.org>
2021-09-14 23:06:15 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 23:06:15 up 19 days, 12:08,  2 users,  load average: 4.29, 3.31, 3.09 > unitime 1631631975241 > 468aa429838df314cf81d08a4a1e670dd8833aaa

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -89,7 +89,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		if (grs > 0x4 || (grs == 0x4 && (sig_grs & 0x1) == 1)) 
 		{
 		    sig_grs += 1;
-		    printf("here\n");
+		    //printf("here\n");
 		}
 		if ((sig_grs >> 23) > 1)
 		{
