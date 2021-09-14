0cb9541ecea53084ff8c966efc73692a581289b4
tracer-pa-public <tracer@njuics.org>
2021-09-14 15:56:43 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 15:56:43 up 19 days,  4:59,  2 users,  load average: 2.91, 2.83, 2.79 > unitime 1631606202928 > eee397a3402a6db9719d44cba189dafae12b1970

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -100,7 +100,6 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 	f.sign = sign;
 	f.exponent = (uint32_t)(exp & 0xff);
 	f.fraction = sig_grs; // here only the lowest 23 bits are kept
-	printf("\033[31m but got: %x = %.f \033[0m \n", f.val, f.fval);
 	return f.val;
 }
 
@@ -141,9 +140,7 @@ uint32_t internal_float_add(uint32_t b, uint32_t a)
 	FLOAT f, fa, fb;
 	fa.val = a;
 	fb.val = b;
-	FLOAT r;
-	r.fval = fa.fval + fb.fval;
-	printf("a = %x = %.f, b = %x = %.f, \033[32m a + b expect: %x = %.f \033[0m \n", fa.val, fa.fval, fb.val, fb.fval, r.val, r.fval);
+	
 	// infity, NaN
 	if (fa.exponent == 0xff)
 	{
