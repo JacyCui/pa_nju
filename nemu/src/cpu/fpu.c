0a581ccd9671d4188760fe516703a1b4a351f152
tracer-pa-public <tracer@njuics.org>
2021-09-14 16:02:48 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 16:02:48 up 19 days,  5:05,  2 users,  load average: 2.79, 2.94, 2.86 > unitime 1631606568316 > 74b4af6c3e3dd0a7f14ebd5b1636563195b17ae1

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -100,6 +100,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 	f.sign = sign;
 	f.exponent = (uint32_t)(exp & 0xff);
 	f.fraction = sig_grs; // here only the lowest 23 bits are kept
+	printf("got: %x = %f", f.val, f.fval);
 	return f.val;
 }
 
@@ -141,8 +142,10 @@ uint32_t internal_float_add(uint32_t b, uint32_t a)
 	fa.val = a;
 	fb.val = b;
 	
+	FLOAT ref;
+	ref.fval = fa.fval + fb.fval;
 	//debug
-	printf("a = %x = %f, b = %x = %f \n", fa.val, fa.fval, fb.val, fb.fval);
+	printf("a = %x = %f, b = %x = %f, a + b = %x = %f\n", fa.val, fa.fval, fb.val, fb.fval, ref.val, ref.fval);
 	
 	// infity, NaN
 	if (fa.exponent == 0xff)
