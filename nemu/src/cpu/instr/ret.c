cf6739cb88aa8fac5ac5ab8a74cd07aa51e65ff9
tracer-pa-public <tracer@njuics.org>
2021-10-04 19:45:55 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 19:45:55 up 39 days,  8:48,  3 users,  load average: 2.74, 2.69, 2.75 > unitime 1633347954925 > bb536ad9ee905cb9463e97c87a8ec569ca4e7f48

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -7,9 +7,9 @@ make_instr_func(ret_near) {
     opr_src.type = OPR_MEM;
     opr_src.addr = cpu.esp;
     opr_src.data_size = data_size;
-    
     operand_read(&opr_src);
-    cpu.esp = opr_src.val;
+    cpu.esp += data_size / 8;
     
+    cpu.esp = opr_src.val;
     return 1;
 }
