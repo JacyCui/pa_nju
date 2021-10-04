4ac34de0c4f5278999b0cba32933a9fbf9b2620e
tracer-pa-public <tracer@njuics.org>
2021-10-04 20:01:27 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 20:01:27 up 39 days,  9:04,  3 users,  load average: 2.57, 2.57, 2.67 > unitime 1633348886776 > 11e6ee6b15cb50c096f3e2e15f6728d5edff260

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -11,6 +11,7 @@ static void instr_execute_1op() {
     opr_dest.data_size = data_size;
     
     operand_read(&opr_src);
+    printf("push: opr_src.val = %x\n", opr_src.val);
     opr_dest.val = opr_src.val;
     operand_write(&opr_dest);
 }
