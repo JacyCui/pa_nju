10939daebeba1eea4677cf0905888ea091a041f1
tracer-pa-public <tracer@njuics.org>
2021-10-04 20:16:29 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 20:16:29 up 39 days,  9:19,  3 users,  load average: 2.51, 2.71, 2.72 > unitime 1633349788613 > b93fb2dec2d62b009511357a68b406ab33d7b927

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -6,7 +6,7 @@ Put the implementations of `call' instructions here.
 static void instr_execute_1op() {
     operand_read(&opr_src);
     cpu.eip += opr_src.val;
-    printf("call eip: %x \n", cpu.eip)
+    printf("call eip: %x \n", cpu.eip);
 }
 
 make_instr_impl_1op(call, i, near);
