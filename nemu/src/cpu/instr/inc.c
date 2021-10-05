ebab1740fa23e239e60e86dfcd2a1a7427cef23f
tracer-pa-public <tracer@njuics.org>
2021-10-05 13:41:05 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 13:41:05 up 40 days,  2:43,  3 users,  load average: 2.40, 2.67, 2.75 > unitime 1633412465637 > 24faf3055f6cd201457ebc358d165532e786411

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -8,7 +8,6 @@ static void instr_execute_1op()
     operand_read(&opr_src);
     opr_src.val = alu_add(opr_src.val, 1, opr_src.data_size);
     operand_write(&opr_src);
-    printf("here\n");
 }
 
 make_instr_impl_1op(inc, rm, v)
--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -7,9 +7,8 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
-	opr_dest.val = alu_sub(opr_src.val, opr_dest.val, opr_src.data_size);
+	opr_dest.val = alu_sub(opr_src.val, opr_dest.val, opr_dest.data_size);
 	operand_write(&opr_dest);
 }
-
 
 make_instr_impl_2op(sub, i, rm, bv)
