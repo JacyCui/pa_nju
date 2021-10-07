d0fc4ba1282ff9b74c0a1b193dd4cf1db9da4418
tracer-pa-public <tracer@njuics.org>
2021-10-07 18:37:01 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 18:37:01 up 42 days,  7:39,  4 users,  load average: 2.69, 2.75, 2.81 > unitime 1633603020996 > e04acdbb7839d7582576f499df4bab441a5caba6

--- nemu/include/cpu/instr/inc.h
+++ nemu/include/cpu/instr/inc.h
@@ -4,6 +4,7 @@
 Put the declarations of `inc' instructions here.
 */
 
+make_instr_func(inc_r_v);
 make_instr_func(inc_rm_v);
 
 
--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -10,4 +10,6 @@ static void instr_execute_1op()
     operand_write(&opr_src);
 }
 
+make_instr_impl_1op(inc, r, v)
 make_instr_impl_1op(inc, rm, v)
+
