76771d6c267877f9d6d344368ccf6d822799f5dc
tracer-pa-public <tracer@njuics.org>
2021-10-11 01:15:35 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 01:15:35 up 45 days, 14:18,  3 users,  load average: 3.00, 3.02, 2.90 > unitime 1633886134815 > 490927bacfe9d6be535cc62ba83d325a196b928b

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -5,19 +5,25 @@ Put the implementations of `call' instructions here.
 
 make_instr_func(call_i_near) 
 {
-    printf("debug: eip = 0x%x ", cpu.eip);
+    printf("debug0: eip = 0x%x \n", cpu.eip);
     int len = 1;
     concat(decode_data_size_, near)
 	concat3(decode_operand, _, i)
 	
+	printf("debug1: eip = 0x%x \n", cpu.eip);
+	
 	// push(eip)
     cpu.esp -= opr_dest.data_size / 8;
+    printf("debug2: eip = 0x%x \n", cpu.eip);
     opr_dest.type = OPR_MEM;
+    printf("debug3: eip = 0x%x \n", cpu.eip);
     opr_dest.addr = cpu.esp;
+    printf("debug4: eip = 0x%x \n", cpu.eip);
     opr_dest.val = cpu.eip + len;
+    printf("debug5: eip = 0x%x \n", cpu.eip);
     operand_write(&opr_dest);
     
-    printf("debug: eip = 0x%x ", cpu.eip);
+    printf("debug6: eip = 0x%x \n", cpu.eip);
     
     // call
     operand_read(&opr_src);
