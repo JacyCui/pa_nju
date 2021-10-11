598b017098785ba2740f5f0cd722c271cb3bd286
tracer-pa-public <tracer@njuics.org>
2021-10-11 11:12:27 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 11:12:27 up 46 days, 15 min,  3 users,  load average: 2.87, 2.84, 2.85 > unitime 1633921946421 > b52c10436c6935bde4beb48f682536e8cf31c4a8

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -5,30 +5,30 @@ Put the implementations of `call' instructions here.
 
 make_instr_func(call_i_near) 
 {
-    //printf("debug0: eip = 0x%x \n", cpu.eip);
+    printf("debug0: eip = 0x%x \n", cpu.eip);
     int len = 1;
     concat(decode_data_size_, near)
 	concat3(decode_operand, _, i)
 	
-	//printf("debug1: eip = 0x%x \n", cpu.eip);
+	printf("debug1: eip = 0x%x \n", cpu.eip);
 	
 	// push(eip)
     cpu.esp -= opr_dest.data_size / 8;
-    //printf("debug2: eip = 0x%x \n", cpu.eip);
+    printf("debug2: eip = 0x%x \n", cpu.eip);
     opr_dest.type = OPR_MEM;
-    //printf("debug3: eip = 0x%x \n", cpu.eip);
+    printf("debug3: eip = 0x%x \n", cpu.eip);
     opr_dest.addr = cpu.esp;
-    //printf("debug4: eip = 0x%x \n", cpu.eip);
+    printf("debug4: eip = 0x%x \n", cpu.eip);
     opr_dest.val = cpu.eip + len;
-    //printf("debug5: eip = 0x%x \n", cpu.eip);
+    printf("debug5: eip = 0x%x \n", cpu.eip);
     operand_write(&opr_dest);
     
-    //printf("debug6: eip = 0x%x \n", cpu.eip);
+    printf("debug6: eip = 0x%x \n", cpu.eip);
     
     // call
     operand_read(&opr_src);
     cpu.eip = resize(cpu.eip + opr_src.val, data_size);
-    //printf("opr_src.val = 0x%x eip = 0x%x\n", opr_src.val, cpu.eip);
+    printf("opr_src.val = 0x%x eip = 0x%x\n", opr_src.val, cpu.eip);
 	                                                                                                    \
 	return len;                                                                                                             
 }
