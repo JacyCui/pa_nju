b5eb369999f2be9da26ef55c0002798bde92a385
tracer-pa-public <tracer@njuics.org>
2021-10-12 11:37:34 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 11:37:34 up 47 days, 40 min,  2 users,  load average: 2.65, 2.74, 2.77 > unitime 1634009854505 > 2018f98f7b79854e2a54ba4ae54e72d312ee36f

--- testcase/src/test-float.c
+++ testcase/src/test-float.c
@@ -5,24 +5,24 @@ int main()
 
 	float a = 1.2, b = 1;
 	float c = a + b;
-	if (-1e-6 < c - 2.2 < 1e-6)
+	if (-1e-6 < c - 2.2 && c - 2.2 < 1e-6)
 		;
 	else
 		HIT_BAD_TRAP;
 	c = a * b;
-	if (-1e-6 < c - 1.2 < 1e-6)
+	if (-1e-6 < c - 1.2 && c - 1.2< 1e-6)
 		;
 	else
 		HIT_BAD_TRAP;
 
 	c = a / b;
-	if (-1e-6 < c - 1.2 < 1e-6)
+	if (-1e-6 < c - 1.2 && c - 1.2 < 1e-6)
 		;
 	else
 		HIT_BAD_TRAP;
 
 	c = a - b;
-	if (-1e-6 < c - 0.2 < 1e-6) // this will fail, and also fails for native program, interesting, can be used as a quiz
+	if (-1e-6 < c - 0.2 && c- 0.2 < 1e-6) // this will fail, and also fails for native program, interesting, can be used as a quiz
 		;
 	else
 		HIT_BAD_TRAP;
