dcc70e9913b2920740b5719d5f8d20fb49f5d4bb
tracer-pa-public <tracer@njuics.org>
2021-10-21 11:18:20 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 11:18:20 up 56 days, 20 min,  2 users,  load average: 2.74, 2.75, 2.80 > unitime 1634786300271 > e2d2c25ee23f0fdd5c5da8ab6162e533564593ab

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,10 +41,10 @@ uint32_t loader()
 			panic("Please implement the loader");
 
 /* TODO: copy the segment from the ELF file to its proper memory area */
-            memcpy(ph->p_vaddr, ph->p_offset, ph->p_filesz);
+            memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_filesz);
 
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
-            memset(ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+            memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
