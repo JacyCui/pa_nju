df7cbd3032628240cebe5bea0c277512dfaa5bf2
tracer-pa-public <tracer@njuics.org>
2021-11-06 21:33:27 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:33:27 up 72 days, 10:36,  2 users,  load average: 3.00, 2.94, 2.92 > unitime 1636205607253 > 3bb91142de0c393a2f617435486ffc5b469bfb36

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -277,7 +277,14 @@ uint32_t eval(int p, int q, bool *success) {
                 case '+': return val1 + val2;
                 case '-': return val1 - val2;
                 case '*': return val1 * val2;
-                case '/': return val1 / val2;
+                case '/': 
+                    if (val2 != 0) {
+                        return val1 / val2;
+                    }
+                    else {
+                        *success = false;
+                        return 0;
+                    }
                 case '^': return val1 ^ val2;
                 case '|': return val1 | val2;
                 case '&': return val1 & val2;
