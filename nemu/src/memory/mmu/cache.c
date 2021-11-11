2dc4cec2097634a41eec6f6eef1206c5e4cd9e91
tracer-pa-public <tracer@njuics.org>
2021-11-11 20:00:23 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 20:00:23 up 77 days,  9:03,  2 users,  load average: 2.87, 2.76, 2.84 > unitime 1636632023494 > 36f97b99f6ecaa6559b09353d8223089b3a3a7b5

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -31,7 +31,7 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data)
                 size_t left = len - cur;
                 for (int j = next_gp; j < next_gp + 8; j++) {
                     if (cache[j].valid_bit && cache[j].tag == addr_state.tag) {
-                        memcpy(cache[j].block, left);
+                        memcpy(cache[j].block, &data, left);
                     }
                 }
 	        }
