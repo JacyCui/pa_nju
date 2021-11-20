d2c5d08260a2240a7879930144a99fac6c6eafee
tracer-pa-public <tracer@njuics.org>
2021-11-20 15:11:18 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 15:11:18 up 86 days,  4:13,  3 users,  load average: 2.66, 3.03, 3.00 > unitime 1637392277907 > e2bc91c9b57c39344b0372853e601c26c77391e7

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -164,7 +164,7 @@ void print_asm_3(char *instr, char *suffix, uint8_t len, OPERAND *opr_1, OPERAND
 // conditions
 // possible condition: e, a, ae, b, be, o, p, s , ne, na, no, np, ns, g, ge, l, le, ecxz
 
-static inline bool inv_cc(const char* info);
+static inline void inv_cc();
 
 #define condition_e \
 	cpu.eflags.ZF
@@ -223,14 +223,11 @@ static inline bool inv_cc(const char* info);
 #define condition_c \
 	cpu.eflags.CF
 
-static inline bool inv_cc(const char* info)
+static inline void inv_cc()
 {
-	printf("lalala Please implement %s condition in instr_helper.h\n", info);
-	printf("here\n");
-	
+	printf("Please implement condition in instr_helper.h\n");
 	fflush(stdout);
 	assert(0);
-	return false;
 }
 
 #endif
--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -7,6 +7,7 @@ static void instr_execute_1op()
 {
     operand_read(&opr_src);
     cpu.gdtr.limit = paddr_read(opr_src.val, 2);
+    printf("debug: %x\n", opr_src.val);
     uint32_t raw_base = paddr_read(opr_src.val + 2, 4);
     cpu.gdtr.base = data_size == 32 ? raw_base : raw_base & 0x00ffffff;
 }
