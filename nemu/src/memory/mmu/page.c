4c377256c74ec56517ac31a8063b5e8bac397084
tracer-pa-public <tracer@njuics.org>
2021-12-04 19:02:44 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 19:02:44 up 100 days,  8:05,  2 users,  load average: 4.04, 3.72, 3.81 > unitime 1638615764406 > 178d701c5f573de295562107efb9e77d61d119

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -10,13 +10,15 @@ paddr_t page_translate(laddr_t laddr)
 // 	assert(0);
 	LADDR_STATUS laddr_status;
 	laddr_status.val = laddr;
-	PDE* pde = (PDE*)(&hw_mem[cpu.cr3.pdbr << 12]) + laddr_status.dir;
+	printf("cpu.cr3.padr = %x \n", (cpu.cr3.pdbr << 12));
+	PDE* pde = (PDE*)(hw_mem + (cpu.cr3.pdbr << 12)) + laddr_status.dir;
 	if (!pde->present) {
 	    printf("\nPage Table Not Present\n");
 	    fflush(stdout);
 	    assert(0);
 	}
-	PTE* pte = (PTE*)(&hw_mem[pde->page_frame << 12]) + laddr_status.page;
+	PTE* pte = (PTE*)(hw_mem + (pde->page_frame << 12)) + laddr_status.page;
+	printf("%x \n", (pde->page_frame << 12));
 	if (!pte->present) {
 	    printf("\nPage Not Present\n");
 	    fflush(stdout);
