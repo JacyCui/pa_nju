4cbf6ab7ab00242fe9771a1bdeaba3fb8ad5e778
tracer-pa-public <tracer@njuics.org>
2021-12-04 18:52:55 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 18:52:55 up 100 days,  7:55,  2 users,  load average: 3.56, 3.92, 3.91 > unitime 1638615174872 > f57a22cbe09a969bba9aa9238cbc0357506b464e

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -11,20 +11,20 @@ paddr_t page_translate(laddr_t laddr)
     printf("debug: here \n");
 	LADDR_STATUS laddr_status;
 	laddr_status.val = laddr;
-	PDE* pde = (PDE*)(hw_mem + (cpu.cr3.pdbr << 12));
-	if (!pde[laddr_status.dir].present) {
+	PDE* pde = (PDE*)(hw_mem + (cpu.cr3.pdbr << 12)) + laddr_status.dir;
+	if (!pde->present) {
 	    printf("\nPage Table Not Present\n");
 	    fflush(stdout);
 	    assert(0);
 	}
-	PTE* pte = (PTE*)(hw_mem + (pde[laddr_status.dir].page_frame << 12));
+	PTE* pte = (PTE*)(hw_mem + (pde[laddr_status.dir].page_frame << 12)) + laddr_status.page;
 	printf("%x \n", (void*)pte - (void*)hw_mem);
-	if (!pte[laddr_status.page].present) {
+	if (!pte->present) {
 	    printf("\nPage Not Present\n");
 	    fflush(stdout);
 	    assert(0);
 	}
-	return (pte[laddr_status.page].page_frame << 12) + laddr_status.offset;
+	return (pte->page_frame << 12) + laddr_status.offset;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 #endif
