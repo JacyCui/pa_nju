2c819a97e8a91b9bffedd4d0b88a42eb062861cf
tracer-pa-public <tracer@njuics.org>
2021-12-29 21:56:35 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:56:35 up 125 days, 10:59,  3 users,  load average: 3.74, 3.90, 3.91 > unitime 1640786195256 > 101f1e06b07cc06c6c95587e2250c1692e43cf1

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,3 +2,23 @@
 /*
 Put the implementations of `lidt' instructions here.
 */
+
+make_instr_func(lidt) {
+    int len = 1;
+    decode_operand_rm
+    
+    // get limit
+    opr_src.data_size = 16;
+    operand_read(&opr_src);
+    cpu.idtr.limit = opr_src.val;
+    
+    // get base
+    opr_src.addr += 2;
+    opr_src.data_size = 32;
+    operand_read(&opr_src);
+    uint32_t raw_base = opr_src.val;
+    cpu.idtr.base = data_size == 32 ? raw_base : raw_base & 0x00ffffff;
+    
+    print_asm_1("lidt", "l", len, &opr_src);
+    return len;
+}
