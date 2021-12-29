634b3019aa3755861885fce3eead0679167bef08
tracer-pa-public <tracer@njuics.org>
2021-12-29 21:47:23 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:47:23 up 125 days, 10:50,  3 users,  load average: 4.18, 3.98, 3.94 > unitime 1640785643541 > 247ea321e89b6182b5de5212b85c3a1567894bf

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -6,13 +6,20 @@ Put the implementations of `lgdt' instructions here.
 make_instr_func(lgdt) {
     int len = 1;
     decode_operand_rm
-    decode_data_size_v
-    operand_read(&opr_src);
     
-    cpu.gdtr.limit = paddr_read(opr_src.val, 2);
-    print_asm_1("lgdt", "l", len, &opr_src);
-    uint32_t raw_base = paddr_read(opr_src.val + 2, 4);
+    // get limit
+    opr_src.data_size = 16;
+    operand_read(&opr_src);
+    cpu.gdtr.limit = opr_src.val;
+    
+    // get base
+    opr_src.addr += 2;
+    opr_src.data_size = 32;
+    operand_read(&opr_src);
+    uint32_t raw_base = opr_src.val;
+
     cpu.gdtr.base = data_size == 32 ? raw_base : raw_base & 0x00ffffff;
+    print_asm_1("lgdt", "l", len, &opr_src);
     
     return len;
 }
