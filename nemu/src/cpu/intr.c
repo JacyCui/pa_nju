cba2f00b2fc551cf8d351f51d0b6ec3f7f301660
tracer-pa-public <tracer@njuics.org>
2021-12-29 22:58:35 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 22:58:35 up 125 days, 12:01,  3 users,  load average: 3.31, 3.66, 3.86 > unitime 1640789915186 > c88337a4e1f67f32d2460bdd79f62171f367348c

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,10 +5,46 @@
 void raise_intr(uint8_t intr_no)
 {
 #ifdef IA32_INTR
-	printf("Please implement raise_intr()");
-	fflush(stdout);
-	assert(0);
-	// GateDesc* idt = (GateDesc*)(hw_mem + cpu.idtr.base) + intr_no;
+// 	printf("Please implement raise_intr()\n");
+// 	fflush(stdout);
+// 	assert(0);
+
+    // push eflags
+    cpu.esp -= 4;
+    opr_dest.type = OPR_MEM;
+    opr_dest.addr = cpu.esp;
+    opr_dest.data_size = 32;
+    opr_dest.val = cpu.eflags.val;
+    operand_write(&opr_dest);
+	// push CS
+    cpu.esp -= 4;
+    opr_dest.type = OPR_MEM;
+    opr_dest.addr = cpu.esp;
+    opr_dest.data_size = 32;
+    opr_dest.val = cpu.cs.val;
+    operand_write(&opr_dest);
+    // push eip
+    cpu.esp -= 4;
+    opr_dest.type = OPR_MEM;
+    opr_dest.addr = cpu.esp;
+    opr_dest.data_size = 32;
+    opr_dest.val = cpu.eip;
+    operand_write(&opr_dest);
+    
+    // Find the IDT entry using 'intr_no'
+	GateDesc* idt = (GateDesc*)(hw_mem + cpu.idtr.base) + intr_no;
+    
+    // Clear IF if it is an interrupt
+    if (idt->type == 0xe) {
+        cpu.eflags.IF = 0;
+    }    
+    
+    // Set CS:EIP to the entry of the interrupt handler
+    cpu.cs.val = idt->selector;
+    cpu.eip = idt->offset_31_16 << 16 + offset_15_0;
+    
+    // reload CS with load_sreg()
+    load_sreg(1);
     
 #endif
 }
