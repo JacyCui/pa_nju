f4a6515244505beda6ce8697ed99b7dfc7978069
tracer-pa-public <tracer@njuics.org>
2021-12-31 17:27:45 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 17:27:45 up 127 days,  6:30,  3 users,  load average: 4.00, 3.92, 3.92 > unitime 1640942865105 > 356b321f9a4e6d38dce4dc7c686528c773718ba9

--- nemu/src/cpu/instr/iret.c
+++ nemu/src/cpu/instr/iret.c
@@ -4,7 +4,7 @@ Put the implementations of `iret' instructions here.
 */
 
 make_instr_func(iret) {
-    // restore: pop eflags, CS,  eip
+    // restore: pop eflags, CS, eip
     cpu.eip = pop();
     cpu.cs.val = pop();
     cpu.eflags.val = pop();
