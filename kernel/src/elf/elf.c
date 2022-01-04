a41ca596fc5733cbf8929236aa9d8ecb58b11793
tracer-pa-public <tracer@njuics.org>
2022-01-04 21:08:04 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:08:04 up 131 days, 10:10,  2 users,  load average: 4.05, 4.11, 4.09 > unitime 1641301683946 > 9f3ce65e3970755e583328839b2c970c9f190d5

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -42,7 +42,7 @@ uint32_t loader()
 #ifdef IA32_PAGE
             uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
             // Log("line43: mm_malloc paddr = 0x%x, ph->p_vaddr = 0x%x, ph->p_memsz = 0x%x", paddr, ph->p_vaddr, ph->p_memsz);
-            printf("line43: mm_malloc paddr = 0x%x, ph->p_vaddr = 0x%x, ph->p_memsz = 0x%x\n", paddr, ph->p_vaddr, ph->p_memsz);
+            printf("line43: mm_malloc paddr = 0x%x, ph->p_vaddr = 0x%x, ph->p_memsz = 0x%x", paddr, ph->p_vaddr, ph->p_memsz);
 #endif
 
 #ifndef HAS_DEVICE_IDE
