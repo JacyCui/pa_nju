a40ceb3b626403a5f6cd4029a077049bb1e29d98
tracer-pa-public <tracer@njuics.org>
2022-01-04 20:52:36 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 20:52:36 up 131 days,  9:55,  2 users,  load average: 3.84, 3.95, 3.93 > unitime 1641300756422 > 7e7a32de2ce296844f15627d1c2dcae662a2cf5e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -47,7 +47,7 @@ uint32_t loader()
 /* TODO: copy the segment from the ELF file to its proper memory area */
             memcpy((void *)paddr, (void *)ph->p_offset, ph->p_filesz);
 #else
-            ide_read((void *)paddr, ph->p_offset, ph->p_filesz);
+            ide_write((void *)paddr, ph->p_offset, ph->p_filesz);
 #endif
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
             memset((void *)paddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
