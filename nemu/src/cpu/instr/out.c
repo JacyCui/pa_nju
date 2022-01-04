cae7c2ba16d51b52ee8c453c1efe8b5a8fe4a786
tracer-pa-public <tracer@njuics.org>
2022-01-04 20:31:53 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 20:31:53 up 131 days,  9:34,  2 users,  load average: 3.82, 3.87, 3.96 > unitime 1641299513296 > c963d519c2329f47e6e3d292e78bb3ba65732915

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -5,11 +5,11 @@ Put the implementations of `out' instructions here.
 */
 
 make_instr_func(out_b) {
-    pio_write(cpu.edx & 0xffff, 8, cpu.eax & 0xff);
+    pio_write(cpu.edx & 0xffff, 1, cpu.eax & 0xff);
     return 1;
 }
 
 make_instr_func(out_v) {
-    pio_write(cpu.edx & 0xffff, data_size, cpu.eax & (0xffffffff >> (32 - data_size)));
+    pio_write(cpu.edx & 0xffff, data_size / 8, cpu.eax & (0xffffffff >> (32 - data_size)));
     return 1;
 }
