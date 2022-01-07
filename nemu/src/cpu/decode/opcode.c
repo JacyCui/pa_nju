254cc3ee68088b05ee7964065678e77faead3e8c
tracer-pa-public <tracer@njuics.org>
2022-01-08 00:07:37 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 00:07:37 up 134 days, 13:10,  3 users,  load average: 3.86, 4.06, 4.15 > unitime 1641571657835 > 4659dce2e67e30ceecbb1e3e43983880c79d762f

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -35,7 +35,7 @@ instr_func opcode_entry[256] = {
     /* 0x7c - 0x7f*/ jl_short_, jge_short_, jle_short_, jg_short_,
     /* 0x80 - 0x83*/ group_1_b, group_1_v, nemu_trap, group_1_bv,
     /* 0x84 - 0x87*/ test_r2rm_b, test_r2rm_v, inv, inv,
-    /* 0x88 - 0x8b*/ mov_r2rm_b, mov_r2rm_v, __ref_mov_rm2r_b, __ref_mov_rm2r_v,
+    /* 0x88 - 0x8b*/ mov_r2rm_b, mov_r2rm_v, /*__ref_*/mov_rm2r_b, /*__ref_*/mov_rm2r_v,
     /* 0x8c - 0x8f*/ inv, lea_rm2r_v, mov_rm2s_w, inv,
     /* 0x90 - 0x93*/ nop, inv, inv, inv,
     /* 0x94 - 0x97*/ inv, inv, inv, inv,
