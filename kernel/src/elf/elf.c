1f6dfa85eeba57c766406fabbc321a4cfeda9903
tracer-pa-public <tracer@njuics.org>
2022-01-08 00:22:14 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux ab45cce5b66d 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 00:22:14 up 134 days, 13:24,  3 users,  load average: 3.76, 4.00, 4.04 > unitime 1641572534652 > 1932b18fa2b9d3c1afc002ae3eefe38f92f38d2d

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,7 +41,7 @@ uint32_t loader()
 			// panic("Please implement the loader");
 #ifdef IA32_PAGE
             uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
-            Log("mm_malloc paddr = 0x%x, ph->p_vaddr = 0x%x, ph->p_memsz = 0x%x", paddr, ph->p_vaddr, ph->p_memsz);
+            // Log("mm_malloc paddr = 0x%x, ph->p_vaddr = 0x%x, ph->p_memsz = 0x%x", paddr, ph->p_vaddr, ph->p_memsz);
 #endif
 
 #ifndef HAS_DEVICE_IDE
