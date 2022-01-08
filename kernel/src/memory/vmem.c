4ba7bfa981c2746fb2ca7d60c192b31b4b8f6652
tracer-pa-public <tracer@njuics.org>
2022-01-09 02:29:15 +0800

    > compile nemu testcase kernel > id 201220014 > user pa201220014 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 02:29:15 up  4:11,  1 user,  load average: 0.11, 0.17, 0.17 > unitime 1641666555908 > 57d29cd9440f6443920e1058164935763b32855

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -7,8 +7,6 @@
 #define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE) // number of page tables to cover the vmem
 #define NR_P (SCR_SIZE / PAGE_SIZE + 1) // number of pages to cover the vmem
 
-PTE vptable[NR_PTE * NR_P] align_to_page; // vmem page tables
-
 PDE *get_updir();
 
 
@@ -21,9 +19,8 @@ void create_video_mapping()
 	 * some page tables to create this mapping.
 	 */
     panic("please implement me");
-    /*PDE *pdir = (PDE *)va_to_pa(get_updir()) + (VMEM_ADDR >> 22);
-	PTE *ptable = (PTE *)va_to_pa(vptable);
-    PTE *pt_origin = (PTE *)(pdir->page_frame << 12);
+    PDE *pdir = (PDE *)va_to_pa(get_updir()) + (VMEM_ADDR >> 22);
+    PTE *ptable = (PTE *)(pdir->page_frame << 12);
 	uint32_t pdir_idx, ptable_idx, pframe_idx;
 
 	pframe_idx = VMEM_ADDR >> 12;
@@ -31,19 +28,13 @@ void create_video_mapping()
 	{
 		for (ptable_idx = 0; ptable_idx < NR_PTE; ptable_idx++)
 		{
-            if (ptable_idx < (VMEM_ADDR >> 12) || ptable_idx >= (VMEM_ADDR >> 12) + NR_P) {
-                ptable->val = pt_origin->val;
-            }
-        	else {
+            if (ptable_idx >= (VMEM_ADDR >> 12) && ptable_idx < (VMEM_ADDR >> 12) + NR_P) {
                 ptable->val = make_pte(pframe_idx << 12);
                 pframe_idx++;
             }
-            pt_origin++;
 			ptable++;
 		}
-		pdir[pdir_idx].val = make_pde(ptable);
-		pdir[pdir_idx + KOFFSET / PT_SIZE].val = make_pde(ptable);
-	}*/
+	}
 }
 
 void video_mapping_write_test()
